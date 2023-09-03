bb36b40a9ce48c29e88302de5ad54e5f84a9da63
Andy Shen <andyshen55@gmail.com>
2023-09-03 02:01:54 -0700

    Refactored sections and changed grid styling

--- app/circle/page.tsx
+++ app/circle/page.tsx
@@ -17,124 +17,162 @@ function HandoutCard(props: {
     <a
       target="_blank"
       href={props.href}
-      className="hover:opacity-50 bg-sky-100 rounded-lg p-4 shadow-lg"
+      className="hover:opacity-60 bg-sky-100 rounded-lg p-4 shadow-lg"
     >
       <p className="mt-1 text-lg font-medium text-gray-900 ">{props.name}</p>
       <h3 className="mt-4 text-base text-gray-700 text-center">{props.desc}</h3>
-      <h3 className="mt-4 text-sm text-gray-400 text-right">{props.topic}</h3>
+      <h3 className="mt-4 text-sm text-gray-500 text-right">{props.topic}</h3>
     </a>
   );
 }
 
-function Gallery() {
+function HandoutGallery() {
   return (
-    <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
-      <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6  xl:grid-cols-4 xl:gap-x-8">
-        {handouts.map((handout, idx) => (
-          <HandoutCard
-            name={handout.name}
-            desc={handout.desc}
-            topic={handout.topic}
-            href={handout.href}
-            key={idx}
-          />
-        ))}
+    <section className="full-bleed">
+      <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+        <div className="grid grid-cols-2 gap-y-10 gap-x-6 lg:grid-cols-4 lg:gap-x-8">
+          {handouts.map((handout, idx) => (
+            <HandoutCard
+              name={handout.name}
+              desc={handout.desc}
+              topic={handout.topic}
+              href={handout.href}
+              key={idx}
+            />
+          ))}
+        </div>
       </div>
-    </div>
+    </section>
   );
 }
 
 function VideoSection() {
   return (
-    <div className="flex items-center justify-center p-8">
-      <ReactPlayer
-        url="https://www.youtube.com/watch?v=qoU44qFJ95E&list=PLNC_Fddtqhottfl9HtRu48wDEvxC4HmAM"
-        className="react-player"
-        controls={false}
-      />
-    </div>
+    <section className="full-bleed">
+      <div className="flex items-center justify-center p-8">
+        <ReactPlayer
+          url="https://www.youtube.com/watch?v=qoU44qFJ95E&list=PLNC_Fddtqhottfl9HtRu48wDEvxC4HmAM"
+          className="react-player"
+          controls={false}
+        />
+      </div>
+    </section>
   );
 }
-export default function CirclePage() {
+
+function SetSection() {
+  return (
+    <section>
+      <p className="mt-4">
+        Once the children were sold on programming, we decided to teach Python
+        through a series of{" "}
+        <a
+          target="_blank"
+          className="text-sky-500 hover:text-amber-400"
+          href="https://drive.google.com/drive/folders/1wGkHiOdH0azXRg1l6AFCng73Ti84ESlA?usp=sharing"
+        >
+          Colab notebooks
+        </a>
+        . Coordinating access to laptops was chaotic, but well worth the effort
+        to let the kids proceed at their own pace.
+      </p>
+      <p className="mt-4">
+        After the cohort had developed basic Python proficiency, we launched
+        into a quarter long group project. The goal was to learn the pure math
+        necessary to model the geometry of the game{" "}
+        <a
+          target="_blank"
+          className="text-sky-500 hover:text-amber-400"
+          href="https://en.wikipedia.org/wiki/Set_(card_game)"
+        >
+          Set
+        </a>
+        , and then program a solver as a class.
+      </p>
+      <p className="mt-2 ">
+        Over the course of 10 weeks we covered: groups, finite fields, vector
+        spaces, as well as Python functions and classes. These topics culminated
+        in a handout modeling{" "}
+        <a
+          target="_blank"
+          className="text-sky-500 hover:text-amber-400"
+          href="https://drive.google.com/file/d/1sE6V4dxndbhc-YPkzJubU9LPOmHadKPv/view?usp=sharing"
+        >
+          Set as a vector space
+        </a>
+        , and a{" "}
+        <a
+          target="_blank"
+          className="text-sky-500 hover:text-amber-400"
+          href="https://github.com/andyshen55/pyset"
+        >
+          class-wide implementation of a solver.
+        </a>
+      </p>
+
+      <p className="mt-4 text-base text-right">
+        <a
+          target="_blank"
+          className="text-sky-500 hover:text-amber-400"
+          href="https://docs.google.com/document/d/1YmcxxgYqi62egp1rkU7uX0T9uiB3ldv9_xfvVmad5oU/edit?usp=sharing"
+        >
+          Game driver design doc
+        </a>
+      </p>
+      <p className="text-base text-right">
+        <a
+          target="_blank"
+          className="text-sky-500 hover:text-amber-400"
+          href="https://www.overleaf.com/read/njcyczwzgvjv"
+        >
+          TeX for the pure math handouts
+        </a>
+      </p>
+    </section>
+  );
+}
+
+function IntroSection() {
+  return (
+    <section>
+      <h1 className="font-semibold text-xl tracking-tighter mb-2">
+        I describe the UCLA Math Circle as a Sunday school for gifted kids.
+      </h1>
+      <p className="">
+        {
+          "As a lead instructor at the Circle, I guided my cohort of students through topics from UCLA's undergraduate math curriculum, redesigned for their cognitive capacities. Take a look at some of my favorite handouts that we taught!"
+        }
+      </p>
+    </section>
+  );
+}
+
+function ProgrammingSection() {
   return (
     <>
-      <section>
-        <h1 className="font-semibold text-xl tracking-tighter mb-2">
-          I describe the UCLA Math Circle as a Sunday school for gifted kids.
-        </h1>
-        <p className="">
-          {
-            "As a lead instructor at the Circle, I guided my cohort of students through topics from UCLA's undergraduate math curriculum, redesigned for their cognitive capacities. Take a look at some of my favorite handouts that we taught!"
-          }
-        </p>
-      </section>
-      <section className="full-bleed">
-        <Gallery />
-      </section>
       <section>
         <h2 className="font-semibold text-lg tracking-tighter mb-2">
           We also experimented with teaching programming.
         </h2>
         <p>
-          I noticed that a lot of the students struggled with the cryptarithms
-          handout. Sensing an opportunity to deviate from the handout-centric
-          math curriculum, I animated some videos to illustrate how a
-          computational approach to problem solving could automate insight.
+          I noticed that a lot of the students struggled with solving
+          cryptarithms. Sensing an opportunity to deviate from the
+          handout-centric math curriculum, I animated some videos to illustrate
+          how a computational approach to problem solving could automate
+          insight.
         </p>
       </section>
-      <section className="full-bleed">
-        <VideoSection />
-      </section>
-      <section>
-        <p className="mt-4">
-          Once the children were sold on programming, we decided to teach Python
-          through a series of{" "}
-          <a
-            target="_blank"
-            className=" text-violet-600 hover:text-teal-400"
-            href="https://drive.google.com/drive/folders/1wGkHiOdH0azXRg1l6AFCng73Ti84ESlA?usp=sharing"
-          >
-            Colab notebooks
-          </a>
-          . Coordinating access to laptops was chaotic, but well worth it to let
-          the kids proceed at their own pace.
-        </p>
-        <p className="mt-4">
-          Once the cohort had developed basic Python proficiency, we launched
-          into a quarter long group project. The goal was to learn the pure math
-          necessary to model the geometry of the game{" "}
-          <a
-            target="_blank"
-            className=" text-violet-600 hover:text-teal-400"
-            href="https://en.wikipedia.org/wiki/Set_(card_game)"
-          >
-            Set
-          </a>
-          , and then program a solver as a class.
-        </p>
-        <p className="mt-2 ">
-          We covered: axioms, groups, modular arithmetic, fields, vector spaces,
-          as well as Python functions and classes.
-        </p>
-        <p className="mt-4 text-base text-right">
-          <a
-            target="_blank"
-            className=" text-violet-600 hover:text-teal-400"
-            href="https://www.overleaf.com/read/njcyczwzgvjv"
-          >
-            TeX for the pure math handouts
-          </a>
-        </p>
-        <p className="text-base text-right">
-          <a
-            target="_blank"
-            className=" text-violet-600 hover:text-teal-400"
-            href="https://github.com/andyshen55/pyset"
-          >
-            Base repo for the PySet solver
-          </a>
-        </p>
-      </section>
+      <VideoSection />
+      <SetSection />
+    </>
+  );
+}
+export default function CirclePage() {
+  return (
+    <>
+      <IntroSection />
+      <HandoutGallery />
+      <ProgrammingSection />
     </>
   );
 }
